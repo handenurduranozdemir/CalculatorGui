24a0db48080533ef73ff80ac35feca4f6c8ec427
handenurduranozdemir <handenurduran@gmail.com>
2025-06-28 01:16:48 +0300

    Calculator GUI

---
+++ src/constants/CommonConstants.java
@@ -0,0 +1,23 @@
+package constants;
+
+public class CommonConstants {
+    // app configs
+    public static final String APP_NAME = "Calculator";
+    public static final int[] APP_SIZE = {450, 600}; // {width, height}
+
+    // textfield configs
+    public static final int TEXTFIELD_LENGTH = 10;
+    public static final int TEXTFIELD_FONTSIZE = 36;
+    public static final int TEXTFIELD_SPRINGLAYOUT_NORTHPAD = 30;
+    public static final int TEXTFIELD_SPRINGLAYOUT_WESTPAD = 33;
+
+    // button configs
+    public static final int BUTTON_ROWCOUNT = 4;
+    public static final int BUTTON_COLCOUNT = 4;
+    public static final int BUTTON_COUNT = 16;
+    public static final int BUTTON_FONTSIZE = 46;
+    public static final int BUTTON_SPRINGLAYOUT_NORTHPAD = 150;
+    public static final int BUTTON_SPRINGLAYOUT_WESTPAD = 33;
+    public static final int BUTTON_HGAP = 20;
+    public static final int BUTTON_VGAP = 20;
+}
